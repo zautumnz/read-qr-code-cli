362158dc9354f6960fa14dc7f8d3f84574aa1bd4
zacanger <zac@zacanger.com>
2023-08-13 16:33:40 -0600

    feat: the whole damn thing

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # read-qr-code
 
+Tiny CLI for reading QR codes.
+
 --------
 
 ## Installation
 
+`npm i -g read-qr-code`
+
 ## Usage
+
+`read-qr-code foo.png`
+
+Supported filetypes should be the same as
+[JIMP](https://github.com/jimp-dev/jimp). Just a tiny wrapper around JIMP and
+[jsQR](https://github.com/cozmo/jsQR). I threw this together because I'm tired
+of pulling out my phone just to scan a QR code and then text the link to my
+email address so I can open it up on desktop again.
 
 [LICENSE](./LICENSE.md)
--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 #!/usr/bin/env node
 
-const main = () => {
+const jsqr = require('jsqr')
+const jimp = require('jimp')
 
+const usage = () => {
+  console.error(`Usage:
+read-qr-code some-file.png`)
+  process.exit(1)
+}
+
+const formatData = ({ data, height, width }) => ({
+  imageData: new Uint8ClampedArray(data),
+  height,
+  width
+})
+
+const main = () => {
+  if (!process.argv[2]) {
+    usage()
+  } else {
+    jimp.read(process.argv[2])
+      .then(({ bitmap }) => formatData(bitmap))
+      .then(({ imageData, width, height }) => {
+        return jsqr(imageData, width, height)
+      })
+      .then((info) => {
+        console.log(info.data || info?.chunks?.[0]?.text)
+      })
+      .catch((err) => {
+        console.error('Error reading qr code', err)
+      })
+  }
 }
 
 if (!module.parent) {
-    main()
+  main()
 }
